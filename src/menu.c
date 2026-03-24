10ba53c2fc08aad6ee46cab8735677a8e5d7f65f
Hubert Stumble <searcher@mailbox.org>
2026-03-23 22:08:19 -0400

    Last minute fixes

--- src/menu.c
+++ src/menu.c
@@ -82,7 +82,7 @@ void LoadMenuElements()
     inventorySlot = LoadTexture("resources/global/tex/seeingInventorySlot2.png");
     inventoryHoverAnim = LoadTexture("resources/global/tex/seeingInventorySlotSketch.png");
     textbox = LoadTexture("resources/global/tex/textbox.png");
-    bigTextbox = LoadTexture("resources/global/tex/bigTextbox.png");
+    bigTextbox = LoadTexture("resources/global/tex/big-textbox.png");
 
     int frameWidth = hoverAnimTex.width;
     int frameHeight = hoverAnimTex.height / hoverFrameCount;
@@ -507,7 +507,7 @@ void DrawCharacterbox()
     {
         // Bottom RPG style textbox
         DrawTextureEx(
-            bigTextbox,
+            textbox,
             (Vector2){50,
             screenHeight - 230},
             0,
@@ -521,7 +521,7 @@ void DrawCharacterbox()
     else if (currentTextbox.type == TEXTBOX_BOOK)
     {
         DrawTextureEx(
-            textbox,
+            bigTextbox,
             (Vector2){50, 50},
             0,
             1,
@@ -531,13 +531,8 @@ void DrawCharacterbox()
         DrawTextEx(romanica, currentTextbox.text, (Vector2){100,
             120}, 50, 0, BLACK);
 
-        DrawText(
-            "Press R to close the page",
-            screenWidth - 250,
-            screenHeight - 80,
-            18,
-            GRAY
-        );
+        DrawTextEx(romanica, "Press R to close this page.", (Vector2){screenWidth - 600,
+            screenHeight - 200}, 50, 0, DARKGRAY);
     }
 }
 
